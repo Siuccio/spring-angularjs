f3f2cd0d2e6e30b4eb8de881082eb642194c17f5
alessio.baldini <alessio.baldini@noovle.com>
2018-09-24 08:43:33 +0200

    no message

--- src/main/java/it/spring/web/Hello.java
+++ src/main/java/it/spring/web/Hello.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class Hello {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getUserLogged() {
-		
+		asdfasdf
 		return "index";
 	}
 
